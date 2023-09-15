802f16becb988b9957c48c2ec496cf6ee0a8d244
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-09-14 22:04:03 -0600

    route

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import Routes from './components/Routes';
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>My portfolio</h1>
+      <Routes/>
     </div>
   );
 }
